180485c51798723952b3ca0889deeb3d5d9c118b
Tom Etzel <tom@blueearth.net>
2015-06-29 19:56:54 -0500

    can send messages between screens

--- app/src/main/java/me/spetz83/androlearn/MyActivity.java
+++ app/src/main/java/me/spetz83/androlearn/MyActivity.java
@@ -1,16 +1,30 @@
 package me.spetz83.androlearn;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
 
 public class MyActivity extends Activity {
+
+    public static final String EXTRA_MESSAGE = "me.spetz83.androlearn.MESSAGE";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my);
+    }
+
+    public void sendMessage(View view)
+    {
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        EditText editText = (EditText)findViewById(R.id.edit_message);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
     }
 
     @Override
